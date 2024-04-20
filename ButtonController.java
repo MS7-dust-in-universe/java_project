c90a3e8782689541b4fee1e6b11cf8c09cc45635
Chalani Chanchala <110767282+chalanichanchala22@users.noreply.github.com>
2024-04-20 14:27:01 +0530

    Add files via upload

---
+++ ButtonController.java
@@ -0,0 +1,28 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class ButtonController {
+
+    public Button btnM3;
+
+    public void btnM1OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnM2OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnM4OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnM3OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnM5OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnM6OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+}
